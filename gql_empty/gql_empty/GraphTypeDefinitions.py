265591919fabb10de9ad3dbb94c775ae16f28a95
arktocz <arktocz@gmail.com>
2022-11-25 20:53:26 +0100

    What do you call a fish wearing a bowtie?" "Sofishticated

--- gql_empty/gql_empty/GraphTypeDefinitions.py
+++ gql_empty/gql_empty/GraphTypeDefinitions.py
@@ -16,18 +16,52 @@ def AsyncSessionFromInfo(info):
 
 @strawberryA.federation.type(description="""Type for query root""")
 class FacilityGQLModel:
-   
-    @strawberryA.field(description="""Finds an workflow by their id""")
-    async def id(self, info: strawberryA.types.Info) -> Union[str, None]:
+
+    # @classmethod
+    # async def resolve_reference(cls, info: strawberryA.types.Info, id: strawberryA.ID):
+    #     result = await resolveWorkflowById(AsyncSessionFromInfo(info), id)
+    #     result._type_definition = cls._type_definition # little hack :)
+    #     return result
+    #id
+    @strawberryA.field(description="""Finds an facility by their id""")
+    async def id(self, info: strawberryA.types.Info) -> Union[str, None]:#je jedno jestli async, když je to součástí entity
         result = self.id
         return result
+    # @strawberryA.field(description="""Entity primary key""")
+    # def id(self, info: strawberryA.types.Info) -> strawberryA.ID:
+    #     return self.id
+
+    #name
+    # @strawberryA.field(description="""Facility's name""")
+    # def name(self) -> str:
+    #     return self.name
+    #address
+    # @strawberryA.field(description="""Facility's name""")
+    # def address(self) -> str:
+    #     return self.address
+    #valid
+    # @strawberryA.field(description="""is the membership is still valid""")
+    # def valid(self) -> bool:
+    #     return self.valid
+    #startdate-?????bool je špatně
+    #enddate
+    #facilitytype_id
+
+    #capacity
+    # @strawberryA.field(description="""Facility's name""")
+    # def capacity(self) -> int:
+    #     return self.capacity
+    #manager_id
+
+    #master_facility_id
+    #external_id
 
 ###########################################################################################################################
 #
 # zde definujte svuj Query model
 #
 ###########################################################################################################################
- from gql_empty.GraphResolvers import resolveFacilityById
+from gql_empty.GraphResolvers import resolveFacilityById
 @strawberryA.type(description="""Type for query root""")
 class Query:
    
